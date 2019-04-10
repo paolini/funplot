544850389c9c44539b027c62b82e1828c7c7e284
Emanuele Paolini <emanuele.paolini@gmail.com>
2019-04-10 08:19:42 +0200

    aggiunti sistemi!

--- ode.js
+++ ode.js
@@ -1,11 +1,14 @@
-var expr = "y^2 - x";
-var compiled_expr;
+var expr = "";
+var expr_x = "";
+var expr_y = "";
+var system = true;
+var compiled_expr, compiled_expr_x, compiled_expr_y;
 var plot;
 var draw_slope;
 
 var points = [];
 
-function slopeGraph(plot, fun) {
+function slopeGraph(plot, fx, fy) {
   var xmin = plot.x_pixel(0);
   var ymin = plot.y_pixel(plot.height);
   var xmax = plot.x_pixel(plot.width);
@@ -16,35 +19,36 @@ function slopeGraph(plot, fun) {
 
   for (var x=xmin; x < xmax; x+=dx) {
     for (var y=ymin; y < ymax; y+=dy) {
-        var m = fun(x,y);
-        var s = h/Math.sqrt(1.0 + m*m) ;
+        var dx = fx(x, y);
+        var dy = fy(x, y);
+        var s = Math.sqrt(dx*dx + dy*dy);
         plot.ctx.beginPath();
         plot.moveTo(x, y);
-        plot.lineTo(x + s, y + s*m);
+        plot.lineTo(x + dx/s, y + dy/s);
         plot.ctx.stroke();
     }
   }
 }
 
-function odePlot(plot, fun, x0, y0) {
+function odePlot(plot, fx, fy, x0, y0) {
   var xmin = plot.x_pixel(0);
   var ymin = plot.y_pixel(plot.height);
   var xmax = plot.x_pixel(plot.width);
   var ymax = plot.y_pixel(0);
 
   for (dir=1;dir>=-1;dir-=2) {
-    var dt = (xmax-xmin)/1000.0;
+    var dt = (xmax-xmin)/plot.width;
+    var maxstep = plot.width;
     var x = x0;
     var y = y0;
     plot.ctx.beginPath();
     plot.moveTo(x, y);
-    for (;x<=xmax && x>=xmin && y<=ymax && y>=ymin;) {
-      var m = fun(x,y);
-      var r = dir * Math.sqrt(1.0 + m*m);
-      var dx = dt / r;
-      var dy = dt * m / r;
-      x += dx;
-      y += dy;
+    for (var step=0;x<=xmax && x>=xmin && y<=ymax && y>=ymin && step < maxstep; step++) {
+      var dx = fx(x, y);
+      var dy = fy(x, y);
+      var r = dir * dt / Math.sqrt(dx*dx + dy*dy);
+      x += r * dx;
+      y += r * dy;
       plot.lineTo(x, y);
     }
     plot.ctx.stroke();
@@ -58,33 +62,56 @@ function draw() {
     canvas.height = $("#bottom").offset().top - $("#canvas").offset().top;
     canvas.width = window.innerWidth - 10;
 
+    var fx, fy;
+
+    if (system) {
+      fx = function (x, y) { return compiled_expr_x.eval({'x': x, 'y': y});};
+      fy = function (x, y) { return compiled_expr_y.eval({'x': x, 'y': y});};
+    } else {
+      fx = function (x, y) { return 1.0; };
+      fy = function (x, y) { return compiled_expr.eval({'x': x, 'y': y});}
+    }
+
     plot.setCanvas(canvas);
 
     plot.ctx.clearRect (0 ,0 , canvas.width, canvas.height);
     plot.show();
     plot.ctx.lineWidth = 2;
     plot.ctx.strokeStyle = "rgb(200,200,0)";
-    if (draw_slope) slopeGraph(plot, expr_f);
+    if (draw_slope)
+        slopeGraph(plot, fx, fy);
     plot.ctx.lineWidth = 1;
     plot.ctx.strokeStyle = "rgb(66,44,255)";
     for (var i=0; i<points.length; ++i) {
-        odePlot(plot, expr_f, points[i].x, points[i].y);
+        odePlot(plot, fx, fy, points[i].x, points[i].y);
     }
-}
-
-function expr_f(x, y) {
-    return compiled_expr.eval({"x": x, "y": y});
 }
 
 function update() {
-    expr = $("#expr").val();
-    try {
-      compiled_expr = math.compile(expr);
-    } catch(e) {
-      alert(e);
-      return;
+    if (system) {
+      expr_x = $("#expr_x").val();
+      expr_y = $("#expr_y").val();
+      try {
+        compiled_expr_x = math.compile(expr_x);
+        compiled_expr_y = math.compile(expr_y);
+      } catch(e) {
+        alert(e);
+        return;
+      }
+      $("#formula").html('$$\\begin{cases}'
+      + 'x\' = ' + math.parse(expr_x) + '\\\\'
+      + 'y\' = ' + math.parse(expr_y)
+      + '\\end{cases}$$');
+    } else { // equation
+      expr = $("#expr").val();
+      try {
+        compiled_expr = math.compile(expr);
+      } catch(e) {
+        alert(e);
+        return;
+      }
+      $("#formula").html('$$y\' = ' + math.parse(expr.replace(/y/g,'y')).toTex() + '$$');
     }
-    $("#formula").html('$$y\' = ' + math.parse(expr.replace(/y/g,'y')).toTex() + '$$');
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 
     draw();
@@ -96,11 +123,16 @@ function update() {
 
     var reference = plot.getReference();
     var params = {
-    	"expr": expr,
     	"r": reference.radius.toFixed(3),
       "x": reference.xCenter.toFixed(3),
       "y": reference.yCenter.toFixed(3),
       "points": point_string
+    }
+    if (system) {
+      params.fx = expr_x;
+      params.fy = expr_y;
+    } else {
+      params.f = expr;
     }
     setLocationHash(params);
 }
@@ -110,8 +142,15 @@ $(function() {
 
     params = get_querystring_params();
 
-    if (params['expr']) {
-        $("#expr").val(params['expr']);
+    if (params['f']) {
+        $("#expr").val(params['f']);
+        system = false;
+    }
+
+    if (params['fx'] && params['fy']) {
+        $("#expr_x").val(params['fx']);
+        $("#expr_y").val(params['fy']);
+        system = true;
     }
 
     plot = newPlotFromParams(params);
@@ -127,7 +166,28 @@ $(function() {
       }
     }
 
-    $("#expr").keyup(function(event) {
+    $("#select").change(
+      function() {
+          system = ($("#select").val() == "system");
+          if (system) {
+            $(".equation").hide();
+            $(".system").show();
+          } else {
+            $(".system").hide();
+            $(".equation").show();
+          }
+          update();
+      }
+    );
+
+    if (system) {
+      $("#select").val("system");
+    } else {
+      $("#select").val("equation");
+    }
+    $("#select").change();
+
+    $(".expr").keyup(function(event) {
         if (event.keyCode == 13)
             update();
     });
@@ -159,5 +219,5 @@ $(function() {
 
     $(window).resize(update);
 
-    update();
+    // update();
 });
