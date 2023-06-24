eda17d651a75f454419e433f362e0ba4212ab2c8
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-06-24 09:32:49 +0200

    tailwind screen layout

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="w-full h-screen flex">
+        {children}
+      </div>
+      </body>
     </html>
   )
 }
