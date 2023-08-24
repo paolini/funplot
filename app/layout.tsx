44d4dfa80a8bcc98c2d3380126d52816c5447840
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-08-24 14:07:52 +0200

    matomo tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+const env = process.env.NODE_ENV
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,6 +17,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script dangerouslySetInnerHTML={{
+            __html: `/*
+
+
+
+            https://github.com/paolini/funplot/
+             _____  __ __  ____   ____  _       ___   ______                          
+            |     ||  |  ||    \\ |    \\| |     /   \\ |      |                         
+            |   __||  |  ||  _  ||  o  ) |    |     ||      |                         
+            |  |_  |  |  ||  |  ||   _/| |___ |  O  ||_|  |_|                         
+            |   _] |  :  ||  |  ||  |  |     ||     |  |  |                           
+            |  |   |     ||  |  ||  |  |     ||     |  |  |                           
+            |__|    \\__,_||__|__||__|  |_____| \\___/   |__|                           
+                                                                                      
+            https://github.com/paolini/funplot/
+
+
+
+
+*/` + (env == "development" 
+? `// tracker code not included in development 
+  `
+: `// matomo tracking code
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//matomo.matb.it/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '6']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();`)
+          }} />
         <title>FunPlot</title>
       </head>
       <body className={inter.className}>
